bfd0d3cd725edf3b2a81a7ef0815cd5844d5c77f
Aseef Ahmed <aseefahmed@gmail.com>
2023-09-19 15:22:27 +1200

    changes

--- main.tf
+++ main.tf
@@ -9,23 +9,32 @@ resource "aws_vpc" "example" {
 }
 
 # Create an Internet Gateway (IGW)
-resource "aws_internet_gateway" "example" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.example.id
 }
 
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route" "example" {
+  route_table_id         = aws_route_table.rt.id
+  destination_cidr_block = "0.0.0.0/0" 
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
 # Create two private subnets in the VPC
-resource "aws_subnet" "private_subnet1" {
+resource "aws_subnet" "subnet2" {
   vpc_id                  = aws_vpc.example.id
   cidr_block              = "10.0.1.0/24" # Change this to your desired subnet CIDR block
   availability_zone       = "us-east-1a" # Change to your desired availability zone
   map_public_ip_on_launch = false
 }
 
-resource "aws_subnet" "private_subnet2" {
-  vpc_id                  = aws_vpc.example.id
-  cidr_block              = "10.0.2.0/24" # Change this to your desired subnet CIDR block
-  availability_zone       = "us-east-1b" # Change to your desired availability zone
-  map_public_ip_on_launch = false
+
+resource "aws_route_table_association" "rtb1" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.example.id
 }
 
 # Create a security group that allows all traffic
@@ -54,8 +63,8 @@ resource "aws_ecs_task_definition" "nginx_task" {
   family                   = "nginx-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  memory    = 512
-  cpu       = 10
+  cpu                      = 1024
+  memory                   = 2048
   execution_role_arn = aws_iam_role.ecs_execution_role.arn
 
   container_definitions = jsonencode([
@@ -74,7 +83,7 @@ resource "aws_ecs_task_definition" "nginx_task" {
 
 # Create an IAM role for ECS task execution
 resource "aws_iam_role" "ecs_execution_role" {
-  name = "ecs_execution_role"
+  name = "ecs_execution_role_new"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -91,56 +100,60 @@ resource "aws_iam_role" "ecs_execution_role" {
 
 # Create an ECS cluster
 resource "aws_ecs_cluster" "example" {
-  name = "example-cluster"
+  name = "cluster-new"
 }
 
 # Create an ECS service with 3 replicas
 resource "aws_ecs_service" "nginx_service" {
-  name            = "nginx-service"
+  name            = "nginx-service-new"
   cluster         = aws_ecs_cluster.example.id
   task_definition = aws_ecs_task_definition.nginx_task.arn
   launch_type     = "FARGATE"
   desired_count   = 3
   network_configuration {
-    subnets = [aws_subnet.private_subnet1.id, aws_subnet.private_subnet2.id]
+    subnets = [aws_subnet.private_subnet1.id, aws_subnet.subnet2.id]
     security_groups = [aws_security_group.allow_all.id]
+    assign_public_ip = true
   }
 }
 
 # Create an Application Load Balancer (ALB)
 resource "aws_lb" "example" {
-  name               = "example-alb"
+  name               = "alb"
   internal           = false
   load_balancer_type = "application"
-  subnets            = [aws_subnet.private_subnet1.id, aws_subnet.private_subnet2.id]
+  subnets            = [aws_subnet.private_subnet1.id, aws_subnet.subnet2.id]
 }
 
 # Create a target group for the ECS service
 resource "aws_lb_target_group" "nginx_target_group" {
-  name     = "nginx-target-group"
+  name     = "tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.example.id
 }
 
-# Attach the ECS service to the target group
-resource "aws_lb_target_group_attachment" "ecs_attachment" {
-  target_group_arn = aws_lb_target_group.nginx_target_group.arn
-  target_id        = aws_ecs_service.nginx_service.id
+output "result" {
+    value = aws_ecs_service.nginx_service
 }
+# # Attach the ECS service to the target group
+# resource "aws_lb_target_group_attachment" "ecs_attachment" {
+#   target_group_arn = aws_lb_target_group.nginx_target_group.arn
+#   target_id        = aws_ecs_service.nginx_service.id
+# }
 
-# Create a listener for the ALB
-resource "aws_lb_listener" "example" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = 80
-  protocol          = "HTTP"
+# # Create a listener for the ALB
+# resource "aws_lb_listener" "example" {
+#   load_balancer_arn = aws_lb.example.arn
+#   port              = 80
+#   protocol          = "HTTP"
 
-  default_action {
-    type             = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Fixed response content"
-      status_code  = "200"
-    }
-  }
-}
+#   default_action {
+#     type             = "fixed-response"
+#     fixed_response {
+#       content_type = "text/plain"
+#       message_body = "Fixed response content"
+#       status_code  = "200"
+#     }
+#   }
+# }
